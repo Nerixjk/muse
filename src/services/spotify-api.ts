11ca25863ddc7277e53da3bdd258522cade60965
Nerixjk <32194858+Nerixjk@users.noreply.github.com>
2024-04-09 05:54:17 +0100

    Update spotify-api.ts

--- src/services/spotify-api.ts
+++ src/services/spotify-api.ts
@@ -71,7 +71,7 @@ export default class {
     console.log(search.body.tracks.items);
     const searchTrackId = search.body.tracks.items[0].id;
     const recommended = await this.spotify.getRecommendations({ seed_tracks: [searchTrackId], min_popularity: 50, limit: 1 });
-    console.log(recommended);
+    console.log(recommended.body.tracks);
     return this.limitTracks(recommended.body.tracks.items, 1).map(this.toSpotifyTrack);
   }
 
